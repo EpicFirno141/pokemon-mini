36088843a3e2d808a3ca155f04ac4bd918e66763
Kaelan Leahy <kaelan@leahyonline.com>
2023-06-18 21:15:52 -0500

    Added redux routes and home page.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,24 @@
 import React, { useEffect } from 'react';
-
 import { useDispatch, useSelector } from 'react-redux';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Home from '../Home/Home';
 
 function App() {
   const dispatch = useDispatch();
   return (
-    <>
-    </>
+    <Router>
+      <Switch>
+        <Redirect exact from="/" to="/home" />
+        <Route exact path='/home'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import Box from '@mui/material/Box'
+
+function Home(){
+    return(
+        <Box></Box>
+    );
+}
+
+export default Home;
