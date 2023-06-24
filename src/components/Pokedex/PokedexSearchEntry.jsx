96706cb8be2bfffdc55cf84e6bede5dc1ef63cfa
Kaelan Leahy <kaelan@leahyonline.com>
2023-06-23 19:14:48 -0500

    Added more details to searched pokemon.

--- src/components/Pokedex/PokedexSearchEntry.jsx
+++ src/components/Pokedex/PokedexSearchEntry.jsx
@@ -7,13 +7,51 @@ import CardActions from '@mui/material/CardActions';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 function PokedexSearchEntry({searchPokemonInfo}) {
+
+    const listTypes = (types) => {
+        let fullList = `${capitalize(types[0].type.name)}`;
+        for(let i = 1; i < types.length; i++){
+            fullList += `, ${capitalize(types[i].type.name)}`;
+        }
+        return(fullList);
+    }
+
+    const capitalize = (word) => {
+        return(word.charAt(0).toUpperCase() + word.slice(1))
+    }
+
     return(
         <Box>
-            <Card sx={{ mx: 'auto', maxWidth: 200, mt: 10}}>
-                <CardMedia component="img"
-                    image={searchPokemonInfo.image} />
+            <Card sx={{ mx: 'auto', maxWidth: 400, mt: 10}}>
+                <Grid container>
+                    <Grid item>
+                        <CardMedia component="img" height={200}
+                            image={searchPokemonInfo.image} 
+                        />
+                    </Grid>
+                    <Divider orientation='vertical' variant='middle' flexItem />
+                    <Grid>
+                        <CardContent>
+                            <Typography variant='h6'>
+                                National Dex #{searchPokemonInfo.dex_id}
+                            </Typography>
+                            <Typography variant='body1' textTransform={'capitalize'}>
+                                Name: {searchPokemonInfo.name}
+                            </Typography>
+                            <Typography variant='body1'>
+                                Types: {listTypes(searchPokemonInfo.types)}
+                            </Typography>
+                        </CardContent>
+                    </Grid>
+                </Grid>
+                
             </Card>
         </Box>
     );
